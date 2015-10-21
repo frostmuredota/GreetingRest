45415b88ff21a22dd17fc96cde0dc43c975a2056
rduran <rduran@nisum.com>
2015-10-21 14:47:40 -0300

    asdasd

--- src/main/java/com/greeting/controller/GreetingController.java
+++ src/main/java/com/greeting/controller/GreetingController.java
@@ -1,8 +1,15 @@
 package com.greeting.controller;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +18,40 @@ import com.greeting.modelo.Greeting;
 @RequestMapping("GreetingRest")
 @RestController
 public class GreetingController {
-	Greeting greeting = new Greeting("Hello World");
+    Greeting greeting ;
+    Greeting greeting1;
+    Greeting greeting2 ;
+    List<Greeting> mygreetings;
+    
+    public GreetingController(){
+        greeting = new Greeting("Hello World");
+        greeting1= new Greeting("Hello Nisum");
+        greeting2= new Greeting("Bye Nisum");
+        mygreetings = new ArrayList<Greeting>();
+        mygreetings.add(greeting);
+        mygreetings.add(greeting1);
+        mygreetings.add(greeting2);
+    }
+
 	
-@RequestMapping(value = "/greeting", method = GET, produces = { APPLICATION_JSON_VALUE })
-@ResponseBody
- public  Greeting greeting(){
-	 return greeting;
- }
+    @RequestMapping(value = "/get/{id}", method = GET, produces = { APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public ResponseEntity<Greeting> getGreeting(@PathVariable("id") int id) {
+        if(mygreetings.size()-1 < id){
+            return new ResponseEntity<Greeting>(HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<Greeting>(mygreetings.get(id),
+                    HttpStatus.OK);
+        }
+    }
+    @RequestMapping(value = "/delete/{id}", method = DELETE, produces = { APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public ResponseEntity<Greeting> deleteGreeting(@PathVariable("id") int id) {
+        if(mygreetings.size()-1 < id){
+            return new ResponseEntity<Greeting>(HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<Greeting>(mygreetings.remove(id),
+                    HttpStatus.OK);
+        }
+    }
 }
--- src/test/java/com/greeting/GreetingRestApplicationTests.java
+++ src/test/java/com/greeting/GreetingRestApplicationTests.java
@@ -2,16 +2,21 @@ package com.greeting;
 
 
 import static org.hamcrest.Matchers.equalTo;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.greeting.controller.GreetingController;
 import com.jayway.restassured.http.ContentType;
@@ -22,17 +27,53 @@ import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 @SpringApplicationConfiguration(classes = GreetingRestApplication.class)
 @WebAppConfiguration
 public class GreetingRestApplicationTests {
+    private MockMvc mvc;
     @Before
     public void setUp(){
-    	  RestAssuredMockMvc.standaloneSetup(new GreetingController());
+        mvc = MockMvcBuilders.standaloneSetup(new GreetingController()).build();
+        RestAssuredMockMvc.standaloneSetup(new GreetingController());
     }
 	@Test
-	public void testGreeting() {
+	public void testGetGreeting() {
 		RestAssuredMockMvc.given().
 		when().
-		   get("/GreetingRest/greeting").
+		   get("/GreetingRest/get/{id}",0).
 	    then().
+	       statusCode(200).
 	       body("message", equalTo("Hello World"));
 	}
+	@Test
+	public void testNormalGetGreeting()throws Exception{
+	    mvc.perform(
+                get("/GreetingRest/get/{id}",0).accept(
+                        MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Hello World"));
+	}
+	@Test
+    public void testDeleteGreeting() {
+        RestAssuredMockMvc.
+        given().
+           contentType(ContentType.JSON).
+        when().
+           delete("/GreetingRest/delete/{id}",0).
+        then().
+           statusCode(200).
+           body("message", equalTo("Hello World"));
+    }
+	@Test
+    public void testNormalDeleteGreeting()throws Exception{
+        mvc.perform(
+                delete("/GreetingRest/delete/{id}",0).accept(
+                        MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Hello World"));
+    }
+	
+	
 
 }
